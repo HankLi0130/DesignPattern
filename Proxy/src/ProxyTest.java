adc8aeeb653f46c09e8dbca863672601839c13ff
Hank Li <hankli0130@gmail.com>
2019-06-27 11:38:49 +0800

    修正 ProxyTest 程式碼

--- Proxy/src/ProxyTest.java
+++ Proxy/src/ProxyTest.java
@@ -7,7 +7,7 @@ public class ProxyTest {
         block.show(b -> {
             System.out.println("圖示：" + b.getIcon());
             System.out.println("標題：" + b.getTitle());
-            System.out.println("內容：" + b.getTitle());
+            System.out.println("內容：" + b.getContent());
         });
 
     }
